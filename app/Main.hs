1aa8491c364f80f5cccfc25a55f1bc2983950138
Rob Rix <rob.rix@github.com>
2016-09-03 18:50:13 -0400

    Add a CLI flag to let you switch which renderer you want.

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,25 @@
 module Main where
 
 import Control.Monad.Free.Church
-import Data.Foldable
 import Data.List (intercalate)
+import Options.Applicative
 import UI.View
 
+data Arguments = Arguments { format :: Format }
+data Format = String | HTML
+
 main :: IO ()
-main = putStrLn $ toString $ (list [ text "hello", text "world" ] :: View ())
+main = execParser opts >>= run
+  where opts = info (helper <*>
+          (fmap Arguments
+          $   flag String String (long "string" <> short 'S')
+          <|> flag' HTML (long "html" <> short 'H')))
+          (fullDesc <> progDesc "UI playground")
+
+run :: Arguments -> IO ()
+run Arguments{..} = putStrLn $ case format of
+  String -> toString (list [ text "hello", text "world" ] :: View ())
+  HTML -> toHTML (list [ text "hello", text "world" ] :: View ())
 
 toString :: Show a => View a -> String
 toString = intercalate "\n" . iter go . fmap (pure . show)
