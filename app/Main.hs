2308a418a4c29e9b68a54ba1096683584730f96e
Rob Rix <rob.rix@github.com>
2016-09-03 18:33:36 -0400

    Define a naïve String renderer for views.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Control.Monad.Free.Church
+import Data.Foldable
+import UI.View
+
 main :: IO ()
-main = putStrLn "TBD"
+main = traverse_ putStrLn $ toString $ list [ text "hello", text "world" ]
+
+toString :: View [String] -> [String]
+toString = iter $ \case
+  Text s -> [ s ]
+  List vs -> vs >>= fmap ("- " ++)
