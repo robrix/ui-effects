1b6b23e7c996d07eb4869f9621047534028f0f99
Rob Rix <rob.rix@github.com>
2016-09-03 19:21:38 -0400

    :fire: toHTML.
    It’s kind of tricky to illustrate how this is supposed to work without
    an actual request/response model and I don’t feel like firing up a
    server just for this.

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,22 @@
 module Main where
 
 import Control.Monad.Free.Church
-import Data.List (intercalate)
 import Options.Applicative
 import UI.View
 
 data Arguments = Arguments { format :: Format }
-data Format = String | HTML
+data Format = String
 
 main :: IO ()
 main = execParser opts >>= run
   where opts = info (helper <*>
           (fmap Arguments
-          $   flag String String (long "string" <> short 'S')
-          <|> flag' HTML (long "html" <> short 'H')))
+          $   flag String String (long "string" <> short 'S')))
           (fullDesc <> progDesc "UI playground")
 
 run :: Arguments -> IO ()
 run Arguments{..} = case format of
   String -> cli view
-  HTML -> putStrLn $ toHTML view
   where view = list [ text "hello", input >>= text ] :: View ()
 
 cli :: View () -> IO ()
@@ -27,9 +24,3 @@ cli = iterM go
   where go (Text s) = putStrLn s
         go (List vs) = sequence_ vs
         go (Input f) = getLine >>= f
-
-toHTML :: Show a => View a -> String
-toHTML = intercalate "\n" . iter go . fmap (pure . show)
-  where go (Text s) = [ "<p>" ++ s ++ "</p>" ]
-        go (List vs) = "<ul>" : (vs >>= fmap (\ s -> "  <li>" ++ s ++ "</li>")) ++ ["</ul>"]
-        go (Input f) = [ "<input type='text' />" ]
