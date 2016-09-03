6de85c88e0f077a2021a9650fcedc039164c6985
Rob Rix <rob.rix@github.com>
2016-09-03 18:58:22 -0400

    Add Input to ViewF.

--- src/UI/View.hs
+++ src/UI/View.hs
@@ -5,6 +5,7 @@ import Control.Monad.Free.Church
 data ViewF f
   = Text String
   | List [f]
+  | Input (String -> f)
   deriving Functor
 
 type View a = F ViewF a
@@ -14,3 +15,6 @@ text = wrap . Text
 
 list :: [View a] -> View a
 list = wrap . List
+
+input :: View String
+input = wrap $ Input pure
