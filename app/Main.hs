40920f0f9b1da04c08bd32dc05c0a66dc3622147
Rob Rix <rob.rix@github.com>
2016-09-03 18:38:49 -0400

    Define an HTML renderer.

--- app/Main.hs
+++ app/Main.hs
@@ -12,3 +12,8 @@ toString :: Show a => View a -> String
 toString = intercalate "\n" . iter go . fmap (pure . show)
   where go (Text s) = [ s ]
         go (List vs) = vs >>= fmap ("- " ++)
+
+toHTML :: Show a => View a -> String
+toHTML = intercalate "\n" . iter go . fmap (pure . show)
+  where go (Text s) = [ "<p>" ++ s ++ "</p>" ]
+        go (List vs) = "<ul>" : (vs >>= fmap (\ s -> "  <li>" ++ s ++ "</li>")) ++ ["</ul>"]
