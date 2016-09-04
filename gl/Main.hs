d9b9fd656b654000b1cfe87bf1c91fa8d4b5533d
Rob Rix <rob.rix@github.com>
2016-09-03 21:18:08 -0400

    Draw a string instead.

--- gl/Main.hs
+++ gl/Main.hs
@@ -4,9 +4,6 @@ import Control.Monad.Free.Church
 import Graphics.UI.GLUT
 import UI.View as UI
 
-myPoints :: [(GLfloat,GLfloat,GLfloat)]
-myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
-
 main :: IO ()
 main = do
   _ <- getArgsAndInitialize
@@ -23,12 +20,12 @@ reshape size = do
 display :: DisplayCallback
 display = do
   clear [ColorBuffer]
-  renderPrimitive Points $
-     mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+  render $ do
+    text "hello"
   flush
 
-interpret :: View () -> IO ()
-interpret = iterM $ \case
-  UI.Text _ -> pure ()
+render :: View () -> IO ()
+render = iterM $ \case
+  UI.Text s -> Helvetica12 `renderString` s
   UI.List _ -> pure ()
   UI.Input _ -> pure ()
