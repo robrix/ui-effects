5bb48851eb374dccae8591b9d998110fb926ff86
Rob Rix <rob.rix@github.com>
2016-09-03 20:58:22 -0400

    Stub in the GLUT tutorial.

--- gl/Main.hs
+++ gl/Main.hs
@@ -1,4 +1,26 @@
 module Main where
 
+import Graphics.UI.GLUT
+
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+
 main :: IO ()
-main = putStrLn "TBD"
+main = do
+  getArgsAndInitialize
+  createWindow "Hello World"
+  displayCallback $= display
+  reshapeCallback $= Just reshape
+  mainLoop
+
+reshape :: ReshapeCallback
+reshape size = do
+  viewport $= (Position 0 0, size)
+  postRedisplay Nothing
+
+display :: DisplayCallback
+display = do
+  clear [ColorBuffer]
+  renderPrimitive Points $
+     mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+  flush
