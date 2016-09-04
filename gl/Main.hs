a94aa0eebe715f95795a6e7d18a5d50941d48a7e
Rob Rix <rob.rix@github.com>
2016-09-03 21:09:12 -0400

    Stub in an interpreter for views.

--- gl/Main.hs
+++ gl/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Control.Monad.Free.Church
 import Graphics.UI.GLUT
+import UI.View as UI
 
 myPoints :: [(GLfloat,GLfloat,GLfloat)]
 myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
@@ -24,3 +26,9 @@ display = do
   renderPrimitive Points $
      mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
   flush
+
+interpret :: View () -> IO ()
+interpret = iterM $ \case
+  UI.Text _ -> pure ()
+  UI.List _ -> pure ()
+  UI.Input _ -> pure ()
