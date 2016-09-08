a07a362f6cffdad561c00573cf576c7ecc862e09
Rob Rix <rob.rix@github.com>
2016-09-07 23:01:37 -0400

    Check shaders.

--- src/GL/Shader/Fragment.hs
+++ src/GL/Shader/Fragment.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE RecordWildCards #-}
 module GL.Shader.Fragment where
 
+import Control.Exception
+import Control.Monad
+import Data.Typeable
 import Foreign.C.String
 import Foreign.Marshal.Alloc
 import Foreign.Ptr
 import Foreign.Storable
 import Graphics.GL.Core41
-import Graphics.Shader.Fragment
+import Graphics.GL.Types
 
-compile :: Show a => a -> IO ()
+newtype Shader = Shader { unShader :: GLuint }
+
+newtype ShaderException = ShaderException String
+  deriving (Show, Typeable)
+
+compile :: Show a => a -> IO Shader
 compile fragment = do
   shader <- glCreateShader GL_FRAGMENT_SHADER
   withCString (show fragment) $ \ source -> do
@@ -15,3 +24,22 @@ compile fragment = do
       poke p source
       glShaderSource shader 1 p nullPtr
   glCompileShader shader
+  checkShader (Shader shader)
+
+
+checkShader :: Shader -> IO Shader
+checkShader Shader{..} = do
+  success <- alloca $ \ p -> do
+    glGetShaderiv unShader GL_COMPILE_STATUS p
+    peek p
+  when (success == GL_FALSE) $ do
+    l <- alloca $ \ p -> do
+      glGetShaderiv unShader GL_INFO_LOG_LENGTH p
+      peek p
+    log <- allocaBytes (fromIntegral l) $ \ bytes -> do
+      glGetShaderInfoLog unShader l nullPtr bytes
+      peekCString bytes
+    throw $ ShaderException log
+  pure (Shader unShader)
+
+instance Exception ShaderException
