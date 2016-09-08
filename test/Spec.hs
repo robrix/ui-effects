3de7f81a1a0da54744afe7319dfe8175fbf85e2d
Rob Rix <rob.rix@github.com>
2016-09-07 23:08:23 -0400

    Try running these in parallel.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,5 +2,5 @@ import Test.Hspec
 import GL.Shader.Fragment.Spec
 
 main :: IO ()
-main = hspec $ do
+main = hspec . parallel $ do
   describe "GL.Shader.Fragment.Spec" $ GL.Shader.Fragment.Spec.spec
