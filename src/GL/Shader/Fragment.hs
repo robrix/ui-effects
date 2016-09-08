617e37e232f6b620e717bb50693ff128a0c63652
Rob Rix <rob.rix@github.com>
2016-09-07 22:35:09 -0400

    Stub in a wrapper for shader compilation.

---
+++ src/GL/Shader/Fragment.hs
@@ -0,0 +1,16 @@
+module GL.Shader.Fragment where
+
+import Foreign.C.String
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+import Foreign.Storable
+import Graphics.GL.Core41
+import Graphics.Shader.Fragment
+
+compile :: Show a => a -> IO ()
+compile fragment = do
+  shader <- glCreateShader GL_FRAGMENT_SHADER
+  withCString (show fragment) $ \ source -> do
+    alloca $ \ p -> do
+      poke p source
+      glShaderSource shader 1 p nullPtr
