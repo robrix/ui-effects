12938de3868257ff3a5f2bf313d40d8294d62bd8
Rob Rix <rob.rix@github.com>
2016-09-07 23:47:54 -0400

    Flesh out the expectation for the constant colour program.

--- test/GL/Shader/Fragment/Spec.hs
+++ test/GL/Shader/Fragment/Spec.hs
@@ -1,5 +1,6 @@
 module GL.Shader.Fragment.Spec where
 
+import Data.List (intercalate)
 import GL.Shader.Fragment
 import Graphics.Shader.Fragment
 import Linear.V4
@@ -9,4 +10,9 @@ spec :: Spec
 spec = do
   describe "toGLSL" $ do
     it "compiles constants" $
-      toGLSL (setColour (V4 1 0 0 1)) `shouldBe` "#version 410"
+      toGLSL (setColour (V4 1 0 0 1)) `shouldBe` intercalate "\n"
+        [ "#version 410"
+        , "void main(void) {"
+        , "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
+        , "}"
+        ]
