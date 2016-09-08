a549881c6ae9b3a7ebaffc749e9a7f762aa56acd
Rob Rix <rob.rix@github.com>
2016-09-07 23:13:05 -0400

    Test a simple constant shader (incorrectly).

--- test/GL/Shader/Fragment/Spec.hs
+++ test/GL/Shader/Fragment/Spec.hs
@@ -1,6 +1,12 @@
 module GL.Shader.Fragment.Spec where
 
+import GL.Shader.Fragment
+import Graphics.Shader.Fragment
+import Linear.V4
 import Test.Hspec
 
 spec :: Spec
-spec = pure ()
+spec = do
+  describe "toGLSL" $ do
+    it "compiles constants" $
+      toGLSL (setColour (V4 1 0 0 1)) `shouldBe` "#version 410"
