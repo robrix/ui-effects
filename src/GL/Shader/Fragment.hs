ea6031c97b05132b55fbbfaf24392f8e92b9ecf0
Rob Rix <rob.rix@github.com>
2016-09-10 11:15:55 -0400

    Use the error datatype in GLException.

--- src/GL/Shader/Fragment.hs
+++ src/GL/Shader/Fragment.hs
@@ -36,7 +36,7 @@ data GLError
   | Other String
   deriving Show
 
-data GLException = GLException CallStack String
+data GLException = GLException GLError CallStack
   deriving (Typeable)
 
 toGLSL :: Fragment () -> String
@@ -130,21 +130,21 @@ checkStatus get getLog status object = do
     log <- allocaBytes (fromIntegral l) $ \ bytes -> do
       getLog object l nullPtr bytes
       peekCString bytes
-    throw $ GLException callStack log
+    throw $ GLException (Other log) callStack
   pure object
 
 checkGLError :: IO ()
 checkGLError = glGetError >>= \ e -> case e of
   GL_NO_ERROR -> pure ()
-  GL_INVALID_ENUM -> throw $ GLException callStack "Invalid enum"
-  GL_INVALID_VALUE -> throw $ GLException callStack  "Invalid value"
-  GL_INVALID_OPERATION -> throw $ GLException callStack "Invalid operation"
-  GL_INVALID_FRAMEBUFFER_OPERATION -> throw $ GLException callStack "Invalid framebuffer operation"
-  GL_OUT_OF_MEMORY -> throw $ GLException callStack "Out of memory"
-  _ -> throw $ GLException callStack "Unknown exception"
+  GL_INVALID_ENUM -> throw $ GLException InvalidEnum callStack
+  GL_INVALID_VALUE -> throw $ GLException InvalidValue callStack
+  GL_INVALID_OPERATION -> throw $ GLException InvalidOperation callStack
+  GL_INVALID_FRAMEBUFFER_OPERATION -> throw $ GLException InvalidFramebufferOperation callStack
+  GL_OUT_OF_MEMORY -> throw $ GLException OutOfMemory callStack
+  _ -> throw $ GLException (Other "Unknown") callStack
 
 
 instance Show GLException where
-  showsPrec p (GLException s e) = showString "GLException " . showsPrec p e . showChar '\n' . showString (prettyCallStack s)
+  showsPrec p (GLException e s) = showString "GLException " . showsPrec p e . showChar '\n' . showString (prettyCallStack s)
 
 instance Exception GLException
