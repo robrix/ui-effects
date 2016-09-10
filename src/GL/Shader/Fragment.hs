8d4d51ccc25113069dbfa84c055bc511fa0c3ec4
Rob Rix <rob.rix@github.com>
2016-09-10 15:04:32 -0400

    Pass the computed field count to glVertexAttribPointer.

--- src/GL/Shader/Fragment.hs
+++ src/GL/Shader/Fragment.hs
@@ -55,7 +55,7 @@ withVertices vertices body = alloca $ \ p -> do
   glBindVertexArray array
   glEnableVertexAttribArray 0
   glBindBuffer GL_ARRAY_BUFFER vbo
-  glVertexAttribPointer 0 3 GL_FLOAT GL_FALSE 0 nullPtr
+  glVertexAttribPointer 0 fieldCount GL_FLOAT GL_FALSE 0 nullPtr
   body $ GLArray array
 
 withProgram :: (Program -> IO a) -> IO a
