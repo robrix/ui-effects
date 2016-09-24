aeb4f1fe822c4533a934869e461177b678f0d869
Rob Rix <rob.rix@github.com>
2016-09-24 17:43:01 +0900

    Write a tiny setup DSL.

--- src/GL/Setup.hs
+++ src/GL/Setup.hs
@@ -1,1 +1,16 @@
+{-# LANGUAGE GADTs #-}
 module GL.Setup where
+
+import Control.Action
+import Control.Applicative.Free.Freer
+import qualified Linear.V4 as Linear
+
+data Flag = DepthTest
+data Func = Less
+
+data SetupF a where
+  Flag :: Flag -> Bool -> SetupF ()
+  DepthFunc :: Func -> SetupF ()
+  SetClearColour :: Linear.V4 n -> SetupF ()
+
+type Setup = Freer (Action SetupF)
