5d241a636ef3d4bc22919f10b2e731d8d3c8be2f
Rob Rix <rob.rix@github.com>
2016-10-02 08:37:36 -0400

    Implement an interpreter for Draw.

--- src/GL/Draw.hs
+++ src/GL/Draw.hs
@@ -3,8 +3,10 @@ module GL.Draw where
 
 import Control.Action
 import Control.Monad.Free.Freer
+import Data.Bits
 import GL.Array
 import GL.Program
+import Graphics.GL.Core41
 import Linear.V4 as Linear
 
 data Buffer = Colour | Depth | Stencil
@@ -34,3 +36,30 @@ bindVertexArray = liftF . liftAction . BindVertexArray
 
 drawArrays :: Mode -> Int -> Int -> Draw ()
 drawArrays mode from to = liftF (liftAction (DrawArrays mode from to))
+
+
+runDraw :: Draw a -> IO a
+runDraw = iterM $ \ d -> case d of
+  Action (Clear buffers) rest -> do
+    glClear $ foldr (.|.) 0 ((\ b -> case b of
+      Colour -> GL_COLOR_BUFFER_BIT
+      Depth -> GL_DEPTH_BUFFER_BIT
+      Stencil -> GL_STENCIL_BUFFER_BIT) <$> buffers)
+    rest ()
+  Action (UseProgram program) rest -> do
+    glUseProgram (unGLProgram program)
+    rest ()
+  Action (SetUniform program var value) rest -> do
+    setUniformValue program var value
+    rest ()
+  Action (BindVertexArray array) rest -> do
+    glBindVertexArray (unGLArray array)
+    rest ()
+  Action (DrawArrays mode from to) rest -> do
+    glDrawArrays (case mode of
+      Lines -> GL_LINES
+      LineLoop -> GL_LINE_LOOP
+      LineStrip -> GL_LINE_STRIP
+      Triangles -> GL_TRIANGLES
+      TriangleStrip -> GL_TRIANGLE_STRIP) (fromIntegral from) (fromIntegral to)
+    rest ()
