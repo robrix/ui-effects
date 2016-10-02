5b64b3d58280a225de688286bfc03516da7a42a8
Rob Rix <rob.rix@github.com>
2016-10-02 08:23:09 -0400

    Stub in a little functor for a drawing language.

--- src/GL/Draw.hs
+++ src/GL/Draw.hs
@@ -1,1 +1,20 @@
+{-# LANGUAGE GADTs #-}
 module GL.Draw where
+
+import Control.Action
+import Control.Monad.Free.Freer
+import GL.Array
+import GL.Program
+import Linear.V4 as Linear
+
+data Buffer = Colour | Depth | Stencil
+data Mode = Lines | LineLoop | LineStrip | Triangles | TriangleStrip
+
+data DrawF a where
+  Clear :: Buffer -> DrawF ()
+  UseProgram :: GLProgram -> DrawF ()
+  SetUniform :: GLProgram -> String -> Linear.V4 a -> DrawF ()
+  BindVertexArray :: GLArray n -> DrawF ()
+  DrawArrays :: Mode -> Int -> Int -> DrawF ()
+
+type Draw = Freer (Action DrawF)
