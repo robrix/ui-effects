c90025e625448b75a7b0bebdf5c55080be66ebea
Rob Rix <rob.rix@github.com>
2016-10-31 16:13:26 -0400

    Define ApplicativeFree over Ap.

--- src/Control/Applicative/Free/Class.hs
+++ src/Control/Applicative/Free/Class.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE TypeFamilies #-}
 module Control.Applicative.Free.Class where
 
+import Control.Applicative.Free as Ap (Ap, liftAp)
+
 class Applicative f => ApplicativeFree f where
   type Underlying f :: * -> *
 
   liftAp :: Underlying f a -> f a
+
+instance ApplicativeFree (Ap f) where
+  type Underlying (Ap f) = f
+  liftAp = Ap.liftAp
