02cbd871b140a9714eb217712889404a644561f0
Rob Rix <rob.rix@github.com>
2016-10-31 14:26:38 -0400

    Add an eliminator for sums.

--- src/Data/Functor/Sum.hs
+++ src/Data/Functor/Sum.hs
@@ -2,3 +2,8 @@
 module Data.Functor.Sum where
 
 data (f :+: g) a = L (f a) | R (g a)
+
+runSum :: (f a -> r) -> (g a -> r) -> (f :+: g) a -> r
+runSum f g sum = case sum of
+  L a -> f a
+  R a -> g a
