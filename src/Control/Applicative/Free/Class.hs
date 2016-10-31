3f5f4d010f3eda9a0e6064d1f97320384550e5c6
Rob Rix <rob.rix@github.com>
2016-10-31 16:13:14 -0400

    Add a typeclass for free applicatives.

--- src/Control/Applicative/Free/Class.hs
+++ src/Control/Applicative/Free/Class.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE TypeFamilies #-}
 module Control.Applicative.Free.Class where
+
+class Applicative f => ApplicativeFree f where
+  type Underlying f :: * -> *
+
+  liftAp :: Underlying f a -> f a
