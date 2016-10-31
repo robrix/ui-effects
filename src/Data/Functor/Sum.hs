8dd130495c38389b3a88ddb4ca7dfea32f42dc49
Rob Rix <rob.rix@github.com>
2016-10-31 16:21:39 -0400

    Define a Functor instance over sums.

--- src/Data/Functor/Sum.hs
+++ src/Data/Functor/Sum.hs
@@ -7,3 +7,7 @@ runSum :: (f a -> r) -> (g a -> r) -> (f :+: g) a -> r
 runSum f g sum = case sum of
   L a -> f a
   R a -> g a
+
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+  fmap f = runSum (L . fmap f) (R . fmap f)
