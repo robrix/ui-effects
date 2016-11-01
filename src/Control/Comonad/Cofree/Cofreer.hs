da2d4254fb9053149396c32aebe1526e114fc948
Rob Rix <rob.rix@github.com>
2016-11-01 16:47:48 -0400

    Define Cofreer & CofreerF.

--- src/Control/Comonad/Cofree/Cofreer.hs
+++ src/Control/Comonad/Cofree/Cofreer.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE GADTs #-}
 module Control.Comonad.Cofree.Cofreer where
+
+data CofreerF f a b where
+  Cofree :: a -> (x -> b) -> f x -> CofreerF f a b
+
+newtype Cofreer f a = Cofreer { runCofreer :: CofreerF f a (Cofreer f a) }
