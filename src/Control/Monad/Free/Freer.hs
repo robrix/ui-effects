b49f3acfb60f478f87a723f309c56ea206bbf107
Rob Rix <rob.rix@github.com>
2016-11-01 15:37:18 -0400

    Write the Bifunctor instance over FreerF with a case expression.

--- src/Control/Monad/Free/Freer.hs
+++ src/Control/Monad/Free/Freer.hs
@@ -15,8 +15,9 @@ newtype Freer f a = Freer { runFreer :: FreerF f a (Freer f a) }
 -- Instances
 
 instance Bifunctor (FreerF f) where
-  bimap f _ (Pure a) = Pure (f a)
-  bimap _ g (Free t r) = Free (g . t) r
+  bimap f g r = case r of
+    Pure a -> Pure (f a)
+    Free t r -> Free (g . t) r
 
 instance Functor (FreerF f a) where
   fmap = second
