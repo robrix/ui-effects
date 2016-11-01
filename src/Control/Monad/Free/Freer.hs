32e4612e285ed21870b76b16c440252583eab339
Rob Rix <rob.rix@github.com>
2016-11-01 15:30:16 -0400

    FreerF is a Bifunctor.

--- src/Control/Monad/Free/Freer.hs
+++ src/Control/Monad/Free/Freer.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE GADTs #-}
 module Control.Monad.Free.Freer where
 
+import Data.Bifunctor
+
 data FreerF f a b where
   Pure :: a -> FreerF f a b
   Free :: (x -> b) -> f x -> FreerF f a b
 
 newtype Freer f a = Freer { runFreer :: FreerF f a (Freer f a) }
+
+instance Bifunctor (FreerF f) where
+  bimap f _ (Pure a) = Pure (f a)
+  bimap _ g (Free t r) = Free (g . t) r
