df3d3b14de0d19967599cf29b0817578c35eeff5
Rob Rix <rob.rix@github.com>
2016-11-01 15:38:02 -0400

    Define Recursive & Corecursive instances over Freer.

--- src/Control/Monad/Free/Freer.hs
+++ src/Control/Monad/Free/Freer.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE FlexibleInstances, GADTs, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, GADTs, MultiParamTypeClasses, TypeFamilies #-}
 module Control.Monad.Free.Freer where
 
 import Control.Monad ((>=>))
 import Control.Monad.Free.Class
 import Data.Bifunctor
+import Data.Functor.Foldable
 
 data FreerF f a b where
   Pure :: a -> FreerF f a b
@@ -39,3 +40,8 @@ instance Monad (Freer f) where
 
 instance MonadFree f (Freer f) where
   wrap = Freer . Free id
+
+type instance Base (Freer f a) = FreerF f a
+
+instance Recursive (Freer f a) where project = runFreer
+instance Corecursive (Freer f a) where embed = Freer
