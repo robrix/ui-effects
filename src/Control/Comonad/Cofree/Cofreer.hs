76a53467330cf70618ede1ae5a72ae98c7dd0e1e
Rob Rix <rob.rix@github.com>
2016-11-01 16:48:16 -0400

    Define accessors for the head and tail of a CofreerF.

--- src/Control/Comonad/Cofree/Cofreer.hs
+++ src/Control/Comonad/Cofree/Cofreer.hs
@@ -4,4 +4,11 @@ module Control.Comonad.Cofree.Cofreer where
 data CofreerF f a b where
   Cofree :: a -> (x -> b) -> f x -> CofreerF f a b
 
+headF :: CofreerF f a b -> a
+headF (Cofree a _ _) = a
+
+tailF :: Functor f => CofreerF f a b -> f b
+tailF (Cofree _ t r) = t <$> r
+
+
 newtype Cofreer f a = Cofreer { runCofreer :: CofreerF f a (Cofreer f a) }
