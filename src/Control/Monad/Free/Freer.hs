fb4121212f0af024721ace3c60dbdc2cc260c472
Rob Rix <rob.rix@github.com>
2016-11-01 15:29:50 -0400

    Add Freer & FreerF definitions.

--- src/Control/Monad/Free/Freer.hs
+++ src/Control/Monad/Free/Freer.hs
@@ -1,1 +1,8 @@
+{-# LANGUAGE GADTs #-}
 module Control.Monad.Free.Freer where
+
+data FreerF f a b where
+  Pure :: a -> FreerF f a b
+  Free :: (x -> b) -> f x -> FreerF f a b
+
+newtype Freer f a = Freer { runFreer :: FreerF f a (Freer f a) }
