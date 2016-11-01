3430d75b57678de5d191e7032355e9680f22bc69
Rob Rix <rob.rix@github.com>
2016-11-01 16:55:00 -0400

    Add lifted/unlifted Show instances.

--- src/Control/Comonad/Cofree/Cofreer.hs
+++ src/Control/Comonad/Cofree/Cofreer.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE FlexibleInstances, GADTs, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, GADTs, MultiParamTypeClasses, UndecidableInstances #-}
 module Control.Comonad.Cofree.Cofreer where
 
 import Control.Comonad
 import Control.Comonad.Cofree.Class
 import Data.Bifunctor
+import Data.Functor.Classes
 
 data CofreerF f a b where
   Cofree :: a -> (x -> b) -> f x -> CofreerF f a b
@@ -35,3 +36,17 @@ instance Comonad (Cofreer f) where
 
 instance Functor f => ComonadCofree f (Cofreer f) where
   unwrap = tailF . runCofreer
+
+instance (Functor f, Show1 f) => Show2 (CofreerF f) where
+  liftShowsPrec2 sp1 _ sp2 sa2 d (Cofree a t r) = showsTernaryWith sp1 (const showString) (liftShowsPrec sp2 sa2) "Cofree" d a "id" (t <$> r)
+    where showsTernaryWith :: (Int -> a -> ShowS) -> (Int -> b -> ShowS) -> (Int -> c -> ShowS) -> String -> Int -> a -> b -> c -> ShowS
+          showsTernaryWith sp1 sp2 sp3 name d x y z = showParen (d > 10) $ showString name . showChar ' ' . sp1 11 x . showChar ' ' . sp2 11 y . showChar ' ' . sp3 11 z
+
+instance (Functor f, Show1 f) => Show1 (Cofreer f) where
+  liftShowsPrec sp sa d (Cofreer c) = showsUnaryWith (liftShowsPrec2 sp sa (liftShowsPrec sp sa) (liftShowList sp sa)) "Cofreer" d c
+
+instance (Functor f, Show (f (Cofreer f a)), Show a) => Show (Cofreer f a) where
+  showsPrec d (Cofreer c) = showParen (d > 10) $ showString "Cofreer" . showChar ' ' . showsPrec 11 c
+
+instance (Functor f, Show (f b), Show a) => Show (CofreerF f a b) where
+  showsPrec d (Cofree a t r) = showParen (d > 10) $ showString "Cofree" . showChar ' ' . showsPrec 11 a . showString " id " . showsPrec 11 (t <$> r)
