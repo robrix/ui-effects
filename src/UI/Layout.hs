c78044a7d3a56ae6aaf56f16e4edbb961cefe038
Rob Rix <rob.rix@github.com>
2016-11-02 11:16:28 -0400

    Rename fitLayoutTo' to fitLayoutAndAnnotate.

--- src/UI/Layout.hs
+++ src/UI/Layout.hs
@@ -43,8 +43,8 @@ stack = unStack . foldMap Stack
 measureLayout :: Real a => Layout a (Size a) -> Size a
 measureLayout = fromMaybe (Size 0 0) . fitLayoutTo (pure Nothing)
 
-fitLayoutTo' :: Real a => Size (Maybe a) -> Layout a (Size a) -> Maybe (ALayout a (Size a))
-fitLayoutTo' = fitLayoutToAlgebra $ \ (Cofree maxSize runC layout) -> case layout of
+fitLayoutAndAnnotate :: Real a => Size (Maybe a) -> Layout a (Size a) -> Maybe (ALayout a (Size a))
+fitLayoutAndAnnotate = fitLayoutToAlgebra $ \ (Cofree maxSize runC layout) -> case layout of
   Pure size | maxSize `encloses` size -> Just ((fromMaybe <$> size <*> maxSize) `cowrap` Pure size)
   Free runF l -> case l of
     Inset by child -> do
