7f054b1275b754240486283d7b98ae43f5797ae0
Rob Rix <rob.rix@github.com>
2016-11-02 22:20:58 -0400

    :fire: the stroke/fill drawing terms.

--- src/UI/Drawing.hs
+++ src/UI/Drawing.hs
@@ -5,10 +5,6 @@ module UI.Drawing
 , DrawingF(..)
 , Drawing
 , Rendering
-, setStroke
-, setFill
-, stroke
-, fill
 , text
 , clip
 , liftL
@@ -29,28 +25,12 @@ data Shape a = Rectangle (Linear.V2 a) (Linear.V2 a)
 data Colour a = RGBA !a !a !a !a
 
 data DrawingF a f where
-  SetStroke :: Colour a -> DrawingF a f
-  SetFill :: Colour a -> DrawingF a f
-  Stroke :: Shape a -> DrawingF a f
-  Fill :: Shape a -> DrawingF a f
   Text :: Size (Maybe a) -> String -> DrawingF a f
   Clip :: Size a -> f -> DrawingF a f
   deriving (Foldable, Functor)
 
 type Drawing a = Freer (DrawingF a)
 type Rendering a = Freer (Sum (DrawingF a) (LayoutF a))
-
-setStroke :: Colour a -> Drawing a ()
-setStroke c = liftF $ SetStroke c
-
-setFill :: Colour a -> Drawing a ()
-setFill c = liftF $ SetFill c
-
-stroke :: Shape a -> Drawing a ()
-stroke s = liftF $ Stroke s
-
-fill :: Shape a -> Drawing a ()
-fill s = liftF $ Fill s
 
 text :: Size (Maybe a) -> String -> Drawing a ()
 text maxSize = liftF . Text maxSize
