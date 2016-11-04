d3c7af1e162b08d46393074704985788124dd031
Rob Rix <rob.rix@github.com>
2016-11-04 16:47:15 -0400

    :fire: shape → vertices conversion.

--- gl/Main.hs
+++ gl/Main.hs
@@ -10,7 +10,6 @@ import GL.Exception
 import GL.Program
 import GL.Shader
 import GL.Setup
-import qualified Linear.V2 as Linear
 import qualified Linear.V4 as Linear
 import Prelude hiding (IO)
 import System.Exit
@@ -26,14 +25,6 @@ main = runWindow "UI" (\ swap ->
     (putStrLn . displayException :: SomeException -> IO ())
   `finally`
     exitSuccess
-
-vertices :: Num n => Shape n -> [Linear.V4 n]
-vertices (Rectangle (Linear.V2 ax ay) (Linear.V2 bx by)) =
-  [ Linear.V4 ax ay 0 1
-  , Linear.V4 ax by 0 1
-  , Linear.V4 bx ay 0 1
-  , Linear.V4 bx by 0 1
-  ]
 
 rectVertices :: Num a => Rect a -> [Linear.V4 a]
 rectVertices (Rect (Point x y) (Size w h)) =
