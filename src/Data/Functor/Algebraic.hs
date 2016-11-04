78ac733172946144b075c108f2f86ea804b736ed
Rob Rix <rob.rix@github.com>
2016-11-04 08:15:12 -0400

    Add a Bidi type synonym.

--- src/Data/Functor/Algebraic.hs
+++ src/Data/Functor/Algebraic.hs
@@ -1,4 +1,9 @@
 module Data.Functor.Algebraic where
 
+import Control.Comonad.Cofree.Cofreer
+import Control.Monad.Free.Freer
+
 type Algebra functor result = functor result -> result
 type Coalgebra functor seed = seed -> functor seed
+
+type Bidi functor pure state = CofreerF (FreerF functor pure) state
