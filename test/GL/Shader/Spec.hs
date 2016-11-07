27fd3aa4576126072e18d7ec58dd3ae1ce9e3c84
Rob Rix <rob.rix@github.com>
2016-11-07 12:24:46 -0500

    Update the test.

--- test/GL/Shader/Spec.hs
+++ test/GL/Shader/Spec.hs
@@ -2,13 +2,17 @@ module GL.Shader.Spec where
 
 import Data.List (intercalate)
 import GL.Shader
+import Linear.V4 as Linear
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "toGLSL" $ do
     it "compiles constants" $
-      toGLSL (set (out "fragColour") (V4 1 0 0 1.0)) `shouldBe` intercalate "\n"
+      toGLSL (do
+        fragColour <- bind "fragColour"
+        _ <- set fragColour (v4 1 0 0 1.0 :: Shader (Linear.V4 Float))
+        pure id) `shouldBe` intercalate "\n"
         [ "#version 410"
         , "out vec4 fragColour;"
         , "void main(void) {"
