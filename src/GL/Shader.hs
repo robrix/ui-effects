3224ab814a03ebbb587a9d44c8ec91f646c77e13
Rob Rix <rob.rix@github.com>
2016-11-08 12:48:42 -0500

    Revert "Move the Monoid instance over CompilerState to the bottom."
    This reverts commit 2387ae2b30bd05e5f836f983bf893f49b2c228fc.

--- src/GL/Shader.hs
+++ src/GL/Shader.hs
@@ -157,6 +157,10 @@ data UniformVar where
 
 data CompilerState = CompilerState { uniforms :: [UniformVar], program :: ShowS }
 
+instance Monoid CompilerState where
+  mempty = CompilerState [] id
+  mappend (CompilerState u1 s1) (CompilerState u2 s2) = CompilerState (mappend u1 u2) (s1 . s2)
+
 
 toGLSL :: GLSLValue a => Shader a -> String
 toGLSL = ($ "") . (showString "#version 410\n" .) . program . iterFreer toGLSLAlgebra . fmap (CompilerState [] . showsGLSLValue)
@@ -384,7 +388,3 @@ instance (GLSLValue a, IsShader b) => IsShader (Var (Shader a) -> b) where
   toShader' f i = do
     var <- input ('v' : show i)
     toShader' (f var) (succ i)
-
-instance Monoid CompilerState where
-  mempty = CompilerState [] id
-  mappend (CompilerState u1 s1) (CompilerState u2 s2) = CompilerState (mappend u1 u2) (s1 . s2)
