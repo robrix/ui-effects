b45de6415f3f491c153eef84be32b6209ff9832e
Rob Rix <rob.rix@github.com>
2016-11-09 20:54:03 -0500

    Replace drawIO with a MonadIO instance.

--- gl/Main.hs
+++ gl/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import Control.Exception
 import Control.Monad
+import Control.Monad.IO.Class
 import Data.Time.Clock.POSIX
 import GL.Array
 import GL.Draw
@@ -55,7 +56,7 @@ draw matrix time program array = do
 
   useProgram program
 
-  t <- drawIO (realToFrac . snd . (properFraction :: POSIXTime -> (Integer, POSIXTime)) <$> getPOSIXTime)
+  t <- liftIO (realToFrac . snd . (properFraction :: POSIXTime -> (Integer, POSIXTime)) <$> getPOSIXTime)
   setUniform program time (Linear.V4 (sin (t * 2 * pi)) (cos (t * negate 2 * pi)) 0 0)
   setUniform program matrix (orthographic 0 1024 0 768 (negate 1) 1)
 
