e6107a6d93eb788f4ed4ddef60e2c9b017b49dc6
Rob Rix <rob.rix@github.com>
2016-11-11 16:08:10 -0500

    Add a Geometry type.

--- src/GL/Geometry.hs
+++ src/GL/Geometry.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE GADTs #-}
 module GL.Geometry where
 
+import GL.Scalar
+
 data Mode = Points | Lines | LineLoop | LineStrip | Triangles | TriangleStrip
+
+data Geometry a where
+  Geometry :: (Foldable v, GLScalar n) => Mode -> [v n] -> Geometry (v n)
