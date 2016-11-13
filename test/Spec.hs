7b8c9bc76edf8766424013ed7d44cdc8b26cfaf8
Rob Rix <rob.rix@github.com>
2016-11-13 13:53:29 -0500

    Stub in a test of fitLayout.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 import Test.Hspec
 import GL.Shader.Spec
+import UI.Layout.Spec
 
 main :: IO ()
 main = hspec . parallel $ do
-  describe "GL.Shader.Spec" $ GL.Shader.Spec.spec
+  describe "GL.Shader.Spec" GL.Shader.Spec.spec
+  describe "UI.Layout.Spec" UI.Layout.Spec.spec
--- test/UI/Layout/Spec.hs
+++ test/UI/Layout/Spec.hs
@@ -1,1 +1,11 @@
 module UI.Layout.Spec where
+
+import Test.Hspec
+import UI.Geometry
+import UI.Layout
+
+spec :: Spec
+spec = do
+  describe "fitLayout" $ do
+    it "excludes sizes larger than the horizontal maximum" $
+      fitLayout (Size (Just 10) Nothing) (pure (Size 11 11)) `shouldBe` Nothing
