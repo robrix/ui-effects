5146206f48ee02c8e58682777811b1a30e8ae81b
Rob Rix <rob.rix@github.com>
2016-11-13 15:55:51 -0500

    Mirror the horizontal fitLayout property along the vertical axis.

--- test/UI/Layout/Spec.hs
+++ test/UI/Layout/Spec.hs
@@ -11,3 +11,6 @@ spec = do
   describe "fitLayout" $ do
     prop "includes only sizes up to the horizontal maximum" $
       \ maxW w -> isJust (fitLayout (Size (Just maxW) Nothing) (pure (Size (w :: Int) 0))) == (maxW >= w)
+
+    prop "includes only sizes up to the vertical maximum" $
+      \ maxH h -> isJust (fitLayout (Size Nothing (Just maxH)) (pure (Size 0 (h :: Int)))) == (maxH >= h)
