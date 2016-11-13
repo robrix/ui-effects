28bfd41ece30220aa3744086ec5bcfcd7f797ae1
Rob Rix <rob.rix@github.com>
2016-11-13 16:03:34 -0500

    Test insetting along each axis.

--- test/UI/Layout/Spec.hs
+++ test/UI/Layout/Spec.hs
@@ -14,3 +14,10 @@ spec = do
 
     prop "includes only sizes up to the vertical maximum" $
       \ maxH h -> isJust (fitLayout (Size Nothing (Just maxH)) (pure (Size 0 (h :: Int)))) == (maxH >= h)
+
+  describe "inset" $ do
+    prop "insets the horizontal maximum by twice its margin width" $
+      \ maxW w i -> isJust (fitLayout (Size (Just maxW) Nothing) (inset (Size i 0) (pure (Size (w :: Int) 0)))) == (maxW >= w + (2 * i))
+
+    prop "insets the vertical maximum by twice its margin height" $
+      \ maxH h i -> isJust (fitLayout (Size Nothing (Just maxH)) (inset (Size 0 i) (pure (Size 0 (h :: Int))))) == (maxH >= h + (2 * i))
