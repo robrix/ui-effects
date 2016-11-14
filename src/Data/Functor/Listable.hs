d305e3ab86c4b2090eba17c4f85a669707a4bfc4
Rob Rix <rob.rix@github.com>
2016-11-14 12:58:28 -0500

    Define the lifting of Listable to kind * -> * -> *.

--- src/Data/Functor/Listable.hs
+++ src/Data/Functor/Listable.hs
@@ -9,6 +9,8 @@ module Data.Functor.Listable
 , cons6
 , Listable1(..)
 , tiers1
+, Listable2(..)
+, tiers2
 ) where
 
 import Test.LeanCheck
@@ -18,3 +20,10 @@ class Listable1 l where
 
 tiers1 :: (Listable a, Listable1 l) => [[l a]]
 tiers1 = liftTiers tiers
+
+
+class Listable2 l where
+  liftTiers2 :: [[a]] -> [[b]] -> [[l a b]]
+
+tiers2 :: (Listable a, Listable b, Listable2 l) => [[l a b]]
+tiers2 = liftTiers2 tiers tiers
