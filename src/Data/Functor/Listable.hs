f0fa281d41871d438b5d7757821217b39d638b36
Rob Rix <rob.rix@github.com>
2016-11-14 12:48:18 -0500

    Define a lifting of Listable through Listable1.

--- src/Data/Functor/Listable.hs
+++ src/Data/Functor/Listable.hs
@@ -1,9 +1,13 @@
 module Data.Functor.Listable
 ( module LC
 , Listable1(..)
+, tiers1
 ) where
 
 import Test.LeanCheck as LC (Listable(..))
 
 class Listable1 l where
   liftTiers :: [[a]] -> [[l a]]
+
+tiers1 :: (Listable a, Listable1 l) => [[l a]]
+tiers1 = liftTiers tiers
