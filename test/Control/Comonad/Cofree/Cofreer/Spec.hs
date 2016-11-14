c324e87339c626c53a254d27318fd7df3ba28a04
Rob Rix <rob.rix@github.com>
2016-11-14 13:20:56 -0500

    Test the commutativity of Cofreer/CofreerF equality.

--- test/Control/Comonad/Cofree/Cofreer/Spec.hs
+++ test/Control/Comonad/Cofree/Cofreer/Spec.hs
@@ -11,7 +11,13 @@ spec = do
       prop "is reflexive" $
         \ a -> a `shouldBe` (a :: CofreerF Maybe Int Int)
 
+      prop "is commutative" $
+        \ a b -> a == b `shouldBe` b == (a :: CofreerF Maybe Int Int)
+
   describe "Cofreer" $ do
     describe "Eq" $ do
       prop "is reflexive" $
         \ a -> a `shouldBe` (a :: Cofreer Maybe Int)
+
+      prop "is commutative" $
+        \ a b -> a == b `shouldBe` b == (a :: Cofreer Maybe Int)
