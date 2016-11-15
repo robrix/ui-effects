03809052f51c90aa0d2af67ba579eff5e207978d
Rob Rix <rob.rix@github.com>
2016-11-15 10:34:28 -0500

    Pretty-print test expectations.

--- test/Test/Hspec/LeanCheck.hs
+++ test/Test/Hspec/LeanCheck.hs
@@ -4,6 +4,7 @@ module Test.Hspec.LeanCheck where
 import GHC.Stack
 import Test.Hspec.Core.Spec
 import Test.LeanCheck.Core
+import Data.Functor.Pretty
 
 data Property where
   Property :: Testable prop => prop -> Property
@@ -12,16 +13,17 @@ prop :: (HasCallStack, Testable prop) => String -> prop -> Spec
 prop s = it s . Property
 
 data ShouldBe where
-  ShouldBe :: (Eq a, Show a) => a -> a -> ShouldBe
+  ShouldBe :: (Eq a, Pretty a) => a -> a -> ShouldBe
 
 infix 1 `shouldBe`
-shouldBe :: (Eq a, Show a) => a -> a -> ShouldBe
+shouldBe :: (Eq a, Pretty a) => a -> a -> ShouldBe
 shouldBe = ShouldBe
 
 instance Testable ShouldBe where
   resultiers (ShouldBe actual expected) = fmap prependExpectation <$> resultiers (actual == expected)
-    where prependExpectation (strs, False) = (("expected: " ++ show expected ++ "\n but got: " ++ show actual) : strs, False)
+    where prependExpectation (strs, False) = (render (text "expected:" </> pretty expected `above` text " but got:" </> pretty actual) "" : strs, False)
           prependExpectation other = other
+          render = displayS . renderSmart 80
 
 instance Example Property where
   type Arg Property = ()
