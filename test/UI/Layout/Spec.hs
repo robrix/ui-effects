a6dd57c247201d2ca987fc703b87936531fa268b
Rob Rix <rob.rix@github.com>
2016-11-14 23:58:36 -0500

    Use `shouldBe` in place of == in props.

--- test/UI/Layout/Spec.hs
+++ test/UI/Layout/Spec.hs
@@ -10,21 +10,21 @@ spec :: Spec
 spec = do
   describe "fitLayout" $ do
     prop "includes only sizes up to the horizontal maximum" $
-      \ maxW w -> isJust (fitLayout (Size (Just maxW) Nothing) (pure (Size (w :: Int) 0))) == (maxW >= w)
+      \ maxW w -> isJust (fitLayout (Size (Just maxW) Nothing) (pure (Size (w :: Int) 0))) `shouldBe` (maxW >= w)
 
     prop "includes only sizes up to the vertical maximum" $
-      \ maxH h -> isJust (fitLayout (Size Nothing (Just maxH)) (pure (Size 0 (h :: Int)))) == (maxH >= h)
+      \ maxH h -> isJust (fitLayout (Size Nothing (Just maxH)) (pure (Size 0 (h :: Int)))) `shouldBe` (maxH >= h)
 
   describe "inset" $ do
     prop "insets the horizontal maximum by twice its margin width" $
-      \ maxW w i -> isJust (fitLayout (Size (Just maxW) Nothing) (inset (Size i 0) (pure (Size (w :: Int) 0)))) == (maxW >= w + (2 * i))
+      \ maxW w i -> isJust (fitLayout (Size (Just maxW) Nothing) (inset (Size i 0) (pure (Size (w :: Int) 0)))) `shouldBe` (maxW >= w + (2 * i))
 
     prop "insets the vertical maximum by twice its margin height" $
-      \ maxH h i -> isJust (fitLayout (Size Nothing (Just maxH)) (inset (Size 0 i) (pure (Size 0 (h :: Int))))) == (maxH >= h + (2 * i))
+      \ maxH h i -> isJust (fitLayout (Size Nothing (Just maxH)) (inset (Size 0 i) (pure (Size 0 (h :: Int))))) `shouldBe` (maxH >= h + (2 * i))
 
   describe "stack" $ do
     prop "takes the sum of its children’s heights" $
-      \ a b -> height (measureLayoutSize (stack (pure a) (pure (b :: Size Int)))) == height a + height b
+      \ a b -> height (measureLayoutSize (stack (pure a) (pure (b :: Size Int)))) `shouldBe` height a + height b
 
     prop "takes the maximum of its children’s widths" $
-      \ a b -> width (measureLayoutSize (stack (pure (a :: Size Int)) (pure b))) == max (width a) (width b)
+      \ a b -> width (measureLayoutSize (stack (pure (a :: Size Int)) (pure b))) `shouldBe` max (width a) (width b)
