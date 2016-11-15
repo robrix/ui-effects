0fd714eda41a2ebb32502a0c243bb0a465e6935d
Rob Rix <rob.rix@github.com>
2016-11-15 09:35:52 -0500

    Lift Pretty to * -> * -> *.

--- src/Data/Functor/Pretty.hs
+++ src/Data/Functor/Pretty.hs
@@ -7,3 +7,10 @@ class Pretty1 f where
 
 pretty1 :: (Pretty1 f, Pretty a) => f a -> Doc e
 pretty1 = liftPretty pretty
+
+
+class Pretty2 f where
+  liftPretty2 :: (a -> Doc e) -> (b -> Doc e) -> f a b -> Doc e
+
+pretty2 :: (Pretty2 f, Pretty a, Pretty b) => f a b -> Doc e
+pretty2 = liftPretty2 pretty pretty
