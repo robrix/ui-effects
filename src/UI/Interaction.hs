44428ff129f8b5168edbad208cb249a13b79d10f
Rob Rix <rob.rix@github.com>
2016-11-21 15:15:47 -0500

    Add a type synonym for recursive interaction terms.

--- src/UI/Interaction.hs
+++ src/UI/Interaction.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE GADTs #-}
 module UI.Interaction where
 
+import Control.Monad.Free.Freer
 import UI.Geometry
 
 data InteractionF a f where
   Clickable :: Rect a -> f -> InteractionF a f
+
+type Interaction a = Freer (InteractionF a)
