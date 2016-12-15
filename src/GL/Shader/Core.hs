8d7aae1f0b9174088cd4c0d31e78ded3c5d7977f
Rob Rix <rob.rix@github.com>
2016-12-15 11:33:31 -0500

    Add a core expression type.

--- src/GL/Shader/Core.hs
+++ src/GL/Shader/Core.hs
@@ -1,10 +1,39 @@
 {-# LANGUAGE DefaultSignatures, GADTs, ScopedTypeVariables #-}
 module GL.Shader.Core where
 
+import Control.Monad.Free.Freer
 import Data.Foldable (toList)
 import Data.List (intersperse)
 import Data.Proxy
 import qualified Linear.V4 as Linear
+
+data ExprF a where
+  V4 :: GLSLValue a => Linear.V4 a -> ExprF (Linear.V4 a)
+
+  Add :: a -> a -> ExprF a
+  Sub :: a -> a -> ExprF a
+  Mul :: a -> a -> ExprF a
+  Div :: a -> a -> ExprF a
+  Abs :: a -> ExprF a
+  Signum :: a -> ExprF a
+
+  Sin :: a -> ExprF a
+  Cos :: a -> ExprF a
+  Tan :: a -> ExprF a
+  ASin :: a -> ExprF a
+  ACos :: a -> ExprF a
+  ATan :: a -> ExprF a
+  SinH :: a -> ExprF a
+  CosH :: a -> ExprF a
+  TanH :: a -> ExprF a
+  ASinH :: a -> ExprF a
+  ACosH :: a -> ExprF a
+  ATanH :: a -> ExprF a
+
+  Exp :: a -> ExprF a
+  Log :: a -> ExprF a
+
+type Expr = Freer ExprF
 
 data DeclF a where
   In :: GLSLValue a => String -> DeclF a
