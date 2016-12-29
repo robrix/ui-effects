53c1face37cc1fd298b4259de8d135713b6a7405
Rob Rix <rob.rix@github.com>
2016-12-29 14:01:38 -0500

    Test that backgrounds respect nesting.
    Apparently they do not.

--- test/UI/Drawing/Spec.hs
+++ test/UI/Drawing/Spec.hs
@@ -16,3 +16,6 @@ spec = do
 
     it "respects contained insets" $
       renderingBackgroundRects (wrapL (Background (rgba 1 1 1 1) (wrapR (Inset 10 (pure 10))))) `shouldBe` [ Rect (Point 0 0) (Size 30 30) ]
+
+    it "respects nesting" $
+      renderingBackgroundRects (wrapL (Background (rgba 1 1 1 1) (wrapR (Inset 10 (wrapL (Background (rgba 0 0 0 1) (pure 10))))))) `shouldBe` [ Rect (Point 0 0) (Size 30 30), Rect (Point 10 10) (Size 10 10) ]
