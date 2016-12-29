9f16d6346dfc0aa8812841a64b4cd0b617ff2f4d
Rob Rix <rob.rix@github.com>
2016-12-29 14:09:56 -0500

    Test that direct nesting is respected.

--- test/UI/Drawing/Spec.hs
+++ test/UI/Drawing/Spec.hs
@@ -17,5 +17,8 @@ spec = do
     it "respects contained insets" $
       renderingBackgroundRects (wrapL (Background (rgba 1 1 1 1) (wrapR (Inset 10 (pure 10))))) `shouldBe` [ Rect (Point 0 0) (Size 30 30) ]
 
+    it "respects direct nesting" $
+      renderingBackgroundRects (wrapL (Background (rgba 1 1 1 1) (wrapL (Background (rgba 0 0 0 1) (pure 10))))) `shouldBe` [ Rect (Point 0 0) (Size 10 10), Rect (Point 0 0) (Size 10 10) ]
+
     it "respects indirect nesting" $
       renderingBackgroundRects (wrapL (Background (rgba 1 1 1 1) (wrapR (Inset 10 (wrapL (Background (rgba 0 0 0 1) (pure 10))))))) `shouldBe` [ Rect (Point 0 0) (Size 30 30), Rect (Point 10 10) (Size 10 10) ]
