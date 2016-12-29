1622c1c0e4a00e21e660cdc1caa597f27837d40e
Rob Rix <rob.rix@github.com>
2016-12-29 13:44:20 -0500

    Test that renderingBackgroundRects respects containing inset terms.

--- test/UI/Drawing/Spec.hs
+++ test/UI/Drawing/Spec.hs
@@ -10,3 +10,6 @@ spec = do
   describe "renderingBackgroundRects" $ do
     it "produces a rect for background terms" $
       renderingBackgroundRects (wrapL (Background (rgba 1 1 1 1) (pure 10))) `shouldBe` [ Rect (Point 0 0) (Size 10 10) ]
+
+    it "respects containing insets" $ do
+      renderingBackgroundRects (wrapR (Inset 10 (wrapL (Background (rgba 1 1 1 1) (pure 10))))) `shouldBe` [ Rect (Point 10 10) (Size 10 10) ]
