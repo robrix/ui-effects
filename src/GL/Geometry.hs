dd37c2cb7bfcfc0f2b3020a000bf77ac98e960fd
Rob Rix <rob.rix@github.com>
2016-12-30 18:26:43 -0500

    Derive Show instances for ArrayRange & GeometryArray.

--- src/GL/Geometry.hs
+++ src/GL/Geometry.hs
@@ -11,8 +11,10 @@ data Geometry a where
   Geometry :: (Foldable v, GLScalar n) => Mode -> [v n] -> Geometry (v n)
 
 data ArrayRange = ArrayRange { mode :: Mode, firstVertexIndex :: Int, vertexCount :: Int }
+  deriving Show
 
 data GeometryArray n = GeometryArray { geometryRanges :: [ArrayRange], geometryArray :: GLArray n }
+  deriving Show
 
 
 -- Instances
