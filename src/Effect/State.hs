57136f3989860f168a2e2db1dcfb63ace49bdd0d
Rob Rix <rob.rix@me.com>
2017-04-02 15:18:22 -0400

    Define smart constructors for State.

--- src/Effect/State.hs
+++ src/Effect/State.hs
@@ -1,6 +1,15 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts, GADTs #-}
 module Effect.State where
+
+import Control.Monad.Free.Freer
+import Data.Functor.Union
 
 data State state result where
   Get :: State state state
   Put :: state -> State state ()
+
+get :: InUnion fs (State state) => Freer (Union fs) state
+get = inj Get `Then` return
+
+put :: InUnion fs (State state) => state -> Freer (Union fs) ()
+put value = inj (Put value) `Then` return
