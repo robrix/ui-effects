e12dd27a10137003837ed1bb8bb49240c681b9fb
Rob Rix <rob.rix@me.com>
2017-04-02 14:49:12 -0400

    Define a Union datatype.

--- src/Data/Functor/Union.hs
+++ src/Data/Functor/Union.hs
@@ -1,1 +1,6 @@
+{-# LANGUAGE DataKinds, GADTs, PolyKinds, TypeOperators #-}
 module Data.Functor.Union where
+
+data Union (fs :: [k -> *]) (a :: k) where
+  Here :: f a -> Union (f ': fs) a
+  There :: Union fs a -> Union (f ': fs) a
