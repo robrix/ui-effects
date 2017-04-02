844096d07314c5d80c5385a1e23ca9e9b264327c
Rob Rix <rob.rix@me.com>
2017-04-02 14:56:34 -0400

    Define an InUnion typeclass to inject values into & project values out of a Union.

--- src/Data/Functor/Union.hs
+++ src/Data/Functor/Union.hs
@@ -1,6 +1,20 @@
-{-# LANGUAGE DataKinds, GADTs, PolyKinds, TypeOperators #-}
+{-# LANGUAGE DataKinds, FlexibleInstances, GADTs, MultiParamTypeClasses, PolyKinds, TypeOperators #-}
 module Data.Functor.Union where
 
 data Union (fs :: [k -> *]) (a :: k) where
   Here :: f a -> Union (f ': fs) a
   There :: Union fs a -> Union (f ': fs) a
+
+class InUnion (fs :: [k -> *]) (f :: k -> *) where
+  inj :: f a -> Union fs a
+  prj :: Union fs a -> Maybe (f a)
+
+instance {-# OVERLAPPABLE #-} InUnion (f ': fs) f where
+  inj = Here
+  prj (Here f) = Just f
+  prj _ = Nothing
+
+instance {-# OVERLAPPABLE #-} InUnion fs f => InUnion (g ': fs) f where
+  inj = There . inj
+  prj (There fs) = prj fs
+  prj _ = Nothing
